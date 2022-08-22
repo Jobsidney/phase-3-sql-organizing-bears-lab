07ffc1a3e6666f69f57ed5c64241b55237ccfce7
Jobsidney <jobsidney67@gmail.com>
2022-08-22 16:18:00 +0300

    commit tree f511ebc48963c72ef8d5ab710fc19d4e46d375f7

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES(1,'Rowdy',10,'M','Red','high',true);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES(2,'Tabitha',6,'F','grey','high',true);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES(3,'Grinch',2,'M','black','medium',false);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES(4,'Melissa',13,'F','Red','low',true);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES(5,'Wendy',6,'F','black','high',true);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES(6,'Mr. Chocolate',20,'M','Red','low',false);
+
